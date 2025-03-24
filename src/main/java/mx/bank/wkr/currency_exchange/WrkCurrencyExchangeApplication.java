4faad3026d5f82eebe5ee50eac63a11cf35c23f3
e_cjaramillo <e_cjaramillo@bancoppel.com>
2025-03-24 03:38:14 -0600

    first commit

---
+++ src/main/java/mx/bank/wkr/currency_exchange/WrkCurrencyExchangeApplication.java
@@ -0,0 +1,13 @@
+package mx.bank.wkr.currency_exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrkCurrencyExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrkCurrencyExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/bank/wkr/currency_exchange/WrkCurrencyExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.bank.wkr.currency_exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrkCurrencyExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
